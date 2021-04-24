24d584e4ece6015bea1bd0552533e50f0f1c13a0
mbudi <82695909+mbudi@users.noreply.github.com>
2021-04-24 15:37:51 +0700

    Update index.php

--- 4B/index.php
+++ 4B/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+	<meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>
 
 	<title>Dumb Library</title>
 </head>
